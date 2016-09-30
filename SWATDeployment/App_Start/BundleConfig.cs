79bf293cb5de2b9a6a84cb741d574525e3fbe22e
CampDev <ncampuzanoa@unal.edu.co>
2016-09-29 22:16:20 -0500

    Add new JS

--- SWATDeployment/App_Start/BundleConfig.cs
+++ SWATDeployment/App_Start/BundleConfig.cs
@@ -21,7 +21,10 @@ namespace SWATDeployment
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/agency.js",
+                      "~/Scripts/contact_me.js",
+                      "~/Scripts/jqBootstrapValidation.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
